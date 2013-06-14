a71e0fa37336649068a3eb297a477224c57e52d0
Arron Schaar <aschaar@gmail.com>
2013-06-14 11:49:16 -0700

    bump version, renamed component.json to bower, updated .json files

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,15 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
+    connect: {
+      demo: {
+        options:{
+          port: 3001,
+          base: '',
+          keepalive: true
+        }
+      }
+    },
     jshint:{
       all: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js']
     },
@@ -13,31 +22,22 @@ module.exports = function(grunt) {
         }
       }
     },
-    bumpup: ['component.json', 'package.json', 'xtag.json'],
+    bumpup: ['bower.json', 'package.json', 'xtag.json'],
     tagrelease: {
       file: 'package.json',
-      prefix: 'xtag-v',
+      prefix: '',
       commit: true
-    }, 
-    connect:{
-      server: {
-        options: {
-          port: 9001
-        }
-      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-bumpup');
   grunt.loadNpmTasks('grunt-tagrelease');
   grunt.loadNpmTasks('grunt-smush-components');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-
 
   grunt.registerTask('build', ['jshint','smush-components']);
   grunt.registerTask('bump:patch', ['bumpup:patch', 'tagrelease']);
-
 
 };
--- README.md
+++ README.md
@@ -5,7 +5,7 @@ The map element allows you to drop in one tag with your Cloudmade api key and ha
 ## Syntax
 
 ```
-	<x-map id="map_2" key="39d2b66f0d5d49dbb52a5b7ad87aea9b" location="auto"></x-map>
+	<x-map id="map" key="39d2b66f0d5d49dbb52a5b7ad87aea9b" location="auto"></x-map>
 ```
 
 
